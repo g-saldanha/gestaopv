7ee063a9e0aa0a9af9aa944be7812eb5be3066f5
Gabriel Saldanha <gabriel.athayde@unimed.coop.br>
2024-09-16 18:20:15 -0300

    :chart_with_upwards_trend: Separando charts

---
+++ src/metrics/components/form/GoogleAutoComplete.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+import { useMapsLibrary } from '@vis.gl/react-google-maps';
+
+
+interface PlaceAutocompleteProps {
+    onPlaceSelect: (place: google.maps.places.PlaceResult | null) => void;
+}
+
+export default function GoogleAutoComplete({ onPlaceSelect }: PlaceAutocompleteProps) {
+    const [placeAutocomplete, setPlaceAutocomplete] =
+        useState<google.maps.places.Autocomplete | null>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const places = useMapsLibrary('places');
+
+    useEffect(() => {
+        if (!places || !inputRef.current) return;
+
+        const options = {
+            fields: ['geometry', 'name', 'formatted_address']
+        };
+
+        setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+    }, [places]);
+
+    useEffect(() => {
+        if (!placeAutocomplete) return;
+
+        placeAutocomplete.addListener('place_changed', () => {
+            onPlaceSelect(placeAutocomplete.getPlace());
+        });
+    }, [onPlaceSelect, placeAutocomplete]);
+
+    return (
+        <div className="autocomplete-container">
+            <InputText ref={inputRef} />
+        </div>
+    );
+}
